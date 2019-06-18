8f9eb61583d2f7e86a7a8e5972296c485803af7c
akaiHuang <akai@fawstudio.com>
2019-06-18 21:57:21 +0800

    平行世界1改變在電腦上

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ const IndexPage = () => (
   <div>
     <h1>Hi akaihuang</h1>
     <p>歡迎來到咖雌比網站</p>
-    <p>這裡是平行世界1</p>
+    <p>這裡是平行世界1已經改變在電腦上</p>
     <Link to="/page-2/">Go to page 2</Link>
   </div>
 )
