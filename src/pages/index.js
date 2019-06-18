591e562b454fa741b5cca7dbf88649eb9482e9f8
akaiHuang <akai@fawstudio.com>
2019-06-18 22:29:44 +0800

    css練習

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,15 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Hi akaihuang</h1>
-    <p>歡迎來到咖雌比網站</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <div className="Hero">
+      <div className="HeroGroup">
+        <h1>Hi akaihuang</h1>
+        <p>歡迎來到咖雌比網站</p>
+        <p>Now go build something great.</p>
+        <Link to="/page-2/">Go to page 2</Link>
+      </div>
+    </div>
+
   </div>
 )
 
