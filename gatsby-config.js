80092b1b1602e15df806c830c42fee3c44550db5
akaiHuang <akai@fawstudio.com>
2019-06-20 00:58:34 +0800

    contentful使用方式

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,18 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Design+Code 3',
+    description: 'Complete courses about the best tools and design systems. Prototype and build apps with React and Swift. 60 hours of video content and resource materials. No coding experience required.',
+    keywords: 'react course, react for designers, ios development, sketch app, swift app course, arkit 2, after effects, create sketch plugin'
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve:'gatsby-source-contentful',
+      options:{
+        spaceId:'06ar7n77410w',
+        accessToken:'804msLnAcilGJJvD2qCxv_QVzWLTAJmpyL6jB27g0PM'
+      }
+  
+  }
+],
 }
