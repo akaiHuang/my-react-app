6383f0582991773f3ebe5889e781201abaa091cb
akaiHuang <akai@fawstudio.com>
2019-06-19 20:05:34 +0800

    styledComponent

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Card.css'
 
-const Card = props =>(
+const Card = props => (
     <div className="Card">
         <img src={props.image} />
         <h3>{props.title}</h3>
